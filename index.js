5870d6c08e4e160b525dc5507baa690385e4e12d
Ilya <mikhaylov.lmc@gmail.com>
2015-08-12 17:55:56 +0000

    Edited product header

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express')
+var fs = require('fs')
 var app = express()
+var htmlfile = 'bitstarter.html';
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  var html = fs.readFileSync(htmlfile).toString(); 
+    response.send(html);
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
